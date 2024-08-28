65678095c7985e62dd6d372da3194a5a59fa9667
sumita <sumitabalouch@gmail.com>
2024-08-28 18:52:10 +0430

    solved the app.js codes linters errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 // src/App.js
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { addDigit, addOperator, setDecimal, calculate, clear } from './actions';
+import {
+  addDigit,
+  addOperator,
+  setDecimal,
+  calculate,
+  clear,
+} from './actions';
 
 const App = () => {
   const display = useSelector((state) => state.display);
@@ -16,7 +22,7 @@ const App = () => {
       dispatch(clear());
     } else if (value === '.') {
       dispatch(setDecimal());
-    } else if (!isNaN(value)) {
+    } else if (!Number.isNaN(Number(value))) {
       dispatch(addDigit(value));
     }
   };
@@ -25,26 +31,27 @@ const App = () => {
     <div>
       <div className="calculator">
         <div className="display" id="display">{display}</div>
-        <button id="clear" onClick={() => handleClick('AC')}>AC</button>
-        <button id="divide" onClick={() => handleClick('/')}>/</button>
-        <button id="multiply" onClick={() => handleClick('*')}>x</button>
-        <button id="seven" onClick={() => handleClick('7')}>7</button>
-        <button id="eight" onClick={() => handleClick('8')}>8</button>
-        <button id="nine" onClick={() => handleClick('9')}>9</button>
-        <button id="subtract" onClick={() => handleClick('-')}>-</button>
-        <button id="four" onClick={() => handleClick('4')}>4</button>
-        <button id="five" onClick={() => handleClick('5')}>5</button>
-        <button id="six" onClick={() => handleClick('6')}>6</button>
-        <button id="add" onClick={() => handleClick('+')}>+</button>
-        <button id="one" onClick={() => handleClick('1')}>1</button>
-        <button id="two" onClick={() => handleClick('2')}>2</button>
-        <button id="three" onClick={() => handleClick('3')}>3</button>
-        <button id="equals" onClick={() => handleClick('=')}>=</button>
-        <button id="zero" onClick={() => handleClick('0')}>0</button>
-        <button id="decimal" onClick={() => handleClick('.')}>.</button>
+        <button type="button" id="clear" onClick={() => handleClick('AC')}>AC</button>
+        <button type="button" id="divide" onClick={() => handleClick('/')}>/</button>
+        <button type="button" id="multiply" onClick={() => handleClick('*')}>x</button>
+        <button type="button" id="seven" onClick={() => handleClick('7')}>7</button>
+        <button type="button" id="eight" onClick={() => handleClick('8')}>8</button>
+        <button type="button" id="nine" onClick={() => handleClick('9')}>9</button>
+        <button type="button" id="subtract" onClick={() => handleClick('-')}>-</button>
+        <button type="button" id="four" onClick={() => handleClick('4')}>4</button>
+        <button type="button" id="five" onClick={() => handleClick('5')}>5</button>
+        <button type="button" id="six" onClick={() => handleClick('6')}>6</button>
+        <button type="button" id="add" onClick={() => handleClick('+')}>+</button>
+        <button type="button" id="one" onClick={() => handleClick('1')}>1</button>
+        <button type="button" id="two" onClick={() => handleClick('2')}>2</button>
+        <button type="button" id="three" onClick={() => handleClick('3')}>3</button>
+        <button type="button" id="equals" onClick={() => handleClick('=')}>=</button>
+        <button type="button" id="zero" onClick={() => handleClick('0')}>0</button>
+        <button type="button" id="decimal" onClick={() => handleClick('.')}>.</button>
       </div>
       <div className="footer">
-        Designed and Coded By<br />
+        Designed and Coded By
+        <br />
         Sumita Balouch
       </div>
     </div>
