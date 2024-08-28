978edfd4cbe8225815652bdcf9865e8d53c3046d
sumita <sumitabalouch@gmail.com>
2024-08-28 19:07:12 +0430

    solving the index.js probs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';  // Changed from 'react-dom' to 'react-dom/client'
+import ReactDOM from 'react-dom/client';// Changed from 'react-dom' to 'react-dom/client'
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App';
@@ -8,14 +8,14 @@ import './index.css';
 
 const store = createStore(reducer);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));  // Updated to use createRoot
+const root = ReactDOM.createRoot(document.getElementById('root'));// Updated to use createRoot
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // Add this line to prevent linting errors
